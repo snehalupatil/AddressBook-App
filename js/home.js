a0663ac4f59599bc97b0fd75598a39c3cf1df6c5
snehalupatil <patilsnehal4575@gmail.com>
2021-06-19 11:06:43 +0530

    Added: View Address Book details in a Tabular Format

---
+++ js/home.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+
+createInnerHtml = () => {
+    const headerHtml = "<th></th><th>FullName</th><th>Address</th><th>city</th>" +
+                        "<th>State</th><th>Zip Code</th><th>Phone Number</th><th>Actions</th>";
+
+    //Set innerHtml
+    let innerHtml = `${headerHtml}`;
+    let adderessBookList = createAddressBookJSON(); 
+
+    //for loop for printing multiple persons
+    for(const addressBookData of adderessBookList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td></td>
+            <td>${addressBookData._name}</td>
+            <td>${addressBookData._address}</td>
+            <td>${addressBookData._city}</td>
+            <td>${addressBookData._state}</td>
+            <td>${addressBookData._zip}</td>
+            <td>${addressBookData._phone}</td>
+            <td>
+            <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+            <img id="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
+            </td>
+        </tr>
+    `;
+    }
+
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+//create json Object
+const createAddressBookJSON = () => {
+    let personListLoacal = [
+        {
+            _name:'Snehal Patil',
+            _address:'Kranti chowk',
+            _city:'Sangli',
+            _state:'Maharshtra',
+            _phone:'91 9999999999',
+            _zip:'444 444'}
+    ];
+
+    return personListLoacal;
+}
---
+++ js/site_properties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page : "../pages/home.html",
+    add_book_page: "../pages/addressBook_form.html"
+};
